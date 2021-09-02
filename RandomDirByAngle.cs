922dfd559d22e6964d12a3738236b287b80c554a
Roberto Plínio Parreira Junior <39377477+RobertoPlinio@users.noreply.github.com>
2021-09-02 11:29:39 -0300

    Changed from angle radius do angle range

--- RandomDirByAngle.cs
+++ RandomDirByAngle.cs
@@ -7,17 +7,17 @@ public class RandomDirByAngle
     /// </summary>
 
     /// <summary>
-    /// Returns a direction rotated by a random angle based on an angle radius and a origin vector.
+    /// Returns a direction rotated by a random angle based on an angle range and a origin vector.
     /// </summary>
     /// <param name="origin">The direction that will serve as the origin</param>
-    /// <param name="angleRadius">The angle range radius, in degrees</param>
+    /// <param name="angleRange">The range which limits the new angle, in degrees</param>
     /// <returns></returns>
-    public static Vector3 RandomDirByAngle3D(Vector3 origin, float angleRadius) {
-        //no need to be bigger than the entire sphere
-        angleRadius = Mathf.Clamp(angleRadius, -180, 180);
+    public static Vector3 RandomDirByAngle3D(Vector3 origin, float angleRange) {
+        //No need to be bigger than the entire sphere
+        angleRange = Mathf.Clamp(angleRange/2, 0, 360);
 
         float ptAngle = Random.Range(-2 * Mathf.PI, 2 * Mathf.PI);
-        float randomAngleRad = Random.Range(-angleRadius, angleRadius) * Mathf.Deg2Rad;
+        float randomAngleRad = Random.Range(-angleRange, angleRange) * Mathf.Deg2Rad;
 
         Vector3 pt = new Vector3(Mathf.Cos(ptAngle), Mathf.Sin(ptAngle)).normalized * Mathf.Sin(randomAngleRad);
         pt += Vector3.forward * Mathf.Cos(randomAngleRad);
